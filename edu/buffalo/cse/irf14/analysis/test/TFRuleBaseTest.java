1be4eac9347bdf3a07c50d3bd99be4b980e38ca9
flightno23 <girish90@hotmail.com>
2014-09-12 22:45:31 -0400

    added test cases as updated by Nikhil

--- edu/buffalo/cse/irf14/analysis/test/TFRuleBaseTest.java
+++ edu/buffalo/cse/irf14/analysis/test/TFRuleBaseTest.java
@@ -1,8 +1,8 @@
 package edu.buffalo.cse.irf14.analysis.test;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import edu.buffalo.cse.irf14.analysis.Token;
 import edu.buffalo.cse.irf14.analysis.TokenFilter;
 import edu.buffalo.cse.irf14.analysis.TokenFilterFactory;
 import edu.buffalo.cse.irf14.analysis.TokenFilterType;
@@ -11,39 +11,40 @@ import edu.buffalo.cse.irf14.analysis.Tokenizer;
 import edu.buffalo.cse.irf14.analysis.TokenizerException;
 
 public class TFRuleBaseTest {
-	public final String[] runTest(String str) throws TokenizerException {
-		// create a new instance of tokenizer
+	public final String[] runTest(TokenFilterType type, String str) throws TokenizerException {
 		Tokenizer tkizer = new Tokenizer();
-		// the tokenizer object tkizer consumes the String I/P and returns a TokenStream tstream
-		// tokenizer -> token -> tokenStream
 		TokenStream tstream = tkizer.consume(str);
-		// create an instance of the TokenfilterFactory class
 		TokenFilterFactory factory = TokenFilterFactory.getInstance();
-		// get the fully constructed tokenfilter instance for Special chars type
-		TokenFilter filter = factory.getFilterByType(TokenFilterType.SPECIALCHARS, tstream);
-		// method to bring the iterator back to the beginning of the stream
-		tstream.reset();
+		TokenFilter filter = factory.getFilterByType(type, tstream);
 		
-		// cycle through tokens in the current token stream and processes them
-		while (tstream.hasNext()) {
-			filter.increment();
+		while (filter.increment()) {
+			//Do nothing :/
 		}
 		
-		// get the processed stream back and reset the iterator to the start position
 		tstream = filter.getStream();
 		tstream.reset();
 		
-		// declare a new arraylist list
 		ArrayList<String> list = new ArrayList<String>();
-		
-		
+		String s;
+		Token t;
+
 		while (tstream.hasNext()) {
-			list.add(tstream.next().toString());
+			t = tstream.next();
+
+			if (t != null) {
+				s = t.toString();
+				
+				if (s!= null && !s.isEmpty())
+					list.add(s);	
+			}
 		}
 		
-		// convert the arraylist of tokens into an array and return it
 		String[] rv = new String[list.size()];
 		rv = list.toArray(rv);
+		tkizer = null;
+		tstream = null;
+		filter = null;
+		list = null;
 		return rv;
 	}
 
